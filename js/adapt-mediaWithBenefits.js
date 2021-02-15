8be5a01a438cf3c8e3d6b3a84899a3d0a5afb80a
Guy Willis <guy.willis@kineo.com>
2021-02-15 11:25:15 +0000

    v5 refactor and less optimisation

--- js/adapt-mediaWithBenefits.js
+++ js/adapt-mediaWithBenefits.js
@@ -1,184 +1,185 @@
 define([
-    'core/js/adapt',
-    'core/js/models/componentModel',
-    'components/adapt-contrib-media/js/adapt-contrib-media'
+  'core/js/adapt',
+  'core/js/models/componentModel',
+  'components/adapt-contrib-media/js/adapt-contrib-media'
 ], function(Adapt, ComponentModel, Media) {
 
-    var MediaWithBenefitsModel = ComponentModel.extend({
-        init:function() {
-            this.set('_media', this.get('_items')[0]._media);
-            this.set('_originalTranscript', this.get('_transcript'));
-        }
-    });
+  var MediaWithBenefitsModel = ComponentModel.extend({
+    init:function() {
+      this.set('_media', this.get('_items')[0]._media);
+      this.set('_originalTranscript', this.get('_transcript'));
+    }
+  });
 
-    var MediaWithBenefitsView = Media.view.extend({
+  var MediaWithBenefitsView = Media.view.extend({
 
-        className: function() {
-            // keep mediawithbenefits visually consistent with the media component
-            var str = Media.view.prototype.className.apply(this, arguments);
-            str += ' media-component';
-            return str;
-        },
+    className: function() {
+      // keep mediawithbenefits visually consistent with the media component
+      var str = Media.view.prototype.className.apply(this, arguments);
+      str += ' media__component';
+      return str;
+    },
 
-        events: function() {
-            return _.extend({
-                "click .mwb-item": "onItemClicked"
-            }, Media.view.prototype.events);
-        },
+    events: function() {
+      return _.extend({
+        "click .js-mediawithbenefits-item": "onItemClicked"
+      }, Media.view.prototype.events);
+    },
 
-        onMediaElementPlay: function() {
-            Media.view.prototype.onMediaElementPlay.apply(this, arguments);
+    onMediaElementPlay: function() {
+      Media.view.prototype.onMediaElementPlay.apply(this, arguments);
 
-            this.markItemAsPlayed();
-        },
-    
-        onMediaElementEnded: function() {
-            Media.view.prototype.onMediaElementEnded.apply(this, arguments);
+      this.markItemAsPlayed();
+    },
 
-            this.markItemAsWatched();
-        },
+    onMediaElementEnded: function() {
+      Media.view.prototype.onMediaElementEnded.apply(this, arguments);
 
-        onPlayerReady: function() {
-            Media.view.prototype.onPlayerReady.apply(this, arguments);
+      this.markItemAsWatched();
+    },
 
-            this.selectItem(0);
-        },
+    onPlayerReady: function() {
+      Media.view.prototype.onPlayerReady.apply(this, arguments);
 
-        checkCompletion:function() {
-            if (this.model.get('_setCompletionOn') === 'allPlayed' && this.areAllItemsVisited()) {
-                this.setCompletionStatus();
-            } else if (this.model.get('_setCompletionOn') === 'allEnded' && this.areAllItemsWatched()) {
-                this.setCompletionStatus();
-            }
-        },
+      this.selectItem(0);
+    },
 
-        checkCompletionByTranscript:function() {
-            if (this.model.get('_originalTranscript')._setCompletionOnView !== true) return;
+    checkCompletion:function() {
+      if (this.model.get('_setCompletionOn') === 'allPlayed' && this.areAllItemsVisited()) {
+        this.setCompletionStatus();
+      } else if (this.model.get('_setCompletionOn') === 'allEnded' && this.areAllItemsWatched()) {
+        this.setCompletionStatus();
+      }
+    },
 
-            if (this.areAllItemsWatched()) {
-                this.setCompletionStatus();
-            }
-        },
+    checkCompletionByTranscript:function() {
+      if (this.model.get('_originalTranscript')._setCompletionOnView !== true) return;
 
-        areAllItemsVisited:function() {
-            return this.model.get('_items').every(itemCfg => itemCfg._isPlayed);
-        },
+      if (this.areAllItemsWatched()) {
+        this.setCompletionStatus();
+      }
+    },
 
-        areAllItemsWatched:function() {
-            return this.model.get('_items').every(itemCfg => itemCfg._isWatched);
-        },
+    areAllItemsVisited:function() {
+      return this.model.get('_items').every(itemCfg => itemCfg._isPlayed);
+    },
 
-        markItemAsWatched:function() {
-            var itemCfg = this.model.get('_items')[this.selectedIndex];
-            itemCfg._isWatched = true;
+    areAllItemsWatched:function() {
+      return this.model.get('_items').every(itemCfg => itemCfg._isWatched);
+    },
 
-            this.checkCompletion();
-        },
+    markItemAsWatched:function() {
+      var itemCfg = this.model.get('_items')[this.selectedIndex];
+      itemCfg._isWatched = true;
 
-        markItemAsPlayed:function() {
-            var itemCfg = this.model.get('_items')[this.selectedIndex];
-            itemCfg._isPlayed = true;
+      this.checkCompletion();
+    },
 
-            this.checkCompletion();
-        },
+    markItemAsPlayed:function() {
+      var itemCfg = this.model.get('_items')[this.selectedIndex];
+      itemCfg._isPlayed = true;
 
-        selectItem:function(index) {
-            // get the selected item configuration
-            var itemCfg = this.model.get('_items')[index];
-            // get the selected element
-            var $selectedItem = this.$('.mwb-item').eq(index);
+      this.checkCompletion();
+    },
 
-            // update classes
-            this.$('.mwb-item').removeClass('selected');
-            $selectedItem.addClass('selected');
-            $selectedItem.addClass('visited');
+    selectItem:function(index) {
+      // get the selected item configuration
+      var itemCfg = this.model.get('_items')[index];
+      // get the selected element
+      var $selectedItem = this.$('.js-mediawithbenefits-item').eq(index);
 
-            this.selectedIndex = index;
+      // update classes
+      this.$('.js-mediawithbenefits-item').removeClass('is-selected');
+      $selectedItem.addClass('is-selected');
+      $selectedItem.addClass('is-visited');
 
-            // TODO: add support for Youtube/Vimeo sources
-            this.mediaElement.setSrc(itemCfg._media.mp4);
-            this.mediaElement.load();
+      this.selectedIndex = index;
 
-            // set up transcript
-            if (itemCfg._transcript) {
-                this.model.set('_transcript', _.extend({}, this.model.get('_originalTranscript'), itemCfg._transcript));
-                this.$('.media__transcript-container').html(Handlebars.partials.transcript(this.model.toJSON()));
-            } else {
-                this.model.unset('_transcript');
-                this.$('.media__transcript-container').empty();
-            }
+      // TODO: add support for Youtube/Vimeo sources
+      this.mediaElement.setSrc(itemCfg._media.mp4);
+      this.mediaElement.load();
 
-            // setup poster image
+      // set up transcript
+      if (itemCfg._transcript) {
+        this.model.set('_transcript', _.extend({}, this.model.get('_originalTranscript'), itemCfg._transcript));
+        this.$('.media__transcript-container').html(Handlebars.partials.transcript(this.model.toJSON()));
+      } else {
+        this.model.unset('_transcript');
+        this.$('.media__transcript-container').empty();
+      }
 
-            $(this.mediaElement).attr('poster', itemCfg._media.poster);
-            this.mediaElement.player.setPoster(itemCfg._media.poster);
+      // setup poster image
 
-            var selectedTrack = this.mediaElement.player.selectedTrack;
-            var selectedLang = selectedTrack ? selectedTrack.srclang || 'none' : 'none';
+      $(this.mediaElement).attr('poster', itemCfg._media.poster);
+      this.mediaElement.player.setPoster(itemCfg._media.poster);
 
-            // restore last language selection if applicable
-            this.mediaElement.player.options.startLanguage = selectedLang;
+      var selectedTrack = this.mediaElement.player.selectedTrack;
+      var selectedLang = selectedTrack ? selectedTrack.srclang || 'none' : 'none';
 
-            $(this.mediaElement).find('track').remove();
-            $(this.mediaElement).append(Handlebars.partials.mediaTracks(itemCfg._media.cc));
-            this.mediaElement.player.rebuildtracks();
-        },
+      // restore last language selection if applicable
+      this.mediaElement.player.options.startLanguage = selectedLang;
 
-        playSelection:function() {
-            var $selectedItem = this.$('.mwb-item').eq(this.selectedIndex);
+      $(this.mediaElement).find('track').remove();
+      $(this.mediaElement).append(Handlebars.partials.mediaTracks(itemCfg._media.cc));
+      this.mediaElement.player.rebuildtracks();
+    },
 
-            this.mediaElement.play();
+    playSelection:function() {
+      var $selectedItem = this.$('.js-mediawithbenefits-item').eq(this.selectedIndex);
 
-            $selectedItem.addClass('visited');
-        },
+      this.mediaElement.play();
 
-        onItemClicked:function(e) {
-            var index = $(e.currentTarget).data('index');
+      $selectedItem.addClass('is-visited');
+    },
 
-            if (this.selectedIndex != index) {
-                this.selectItem(index);
-            }
+    onItemClicked:function(e) {
+      var index = $(e.currentTarget).data('index');
 
-            this.playSelection();
-        },
+      if (this.selectedIndex != index) {
+        this.selectItem(index);
+      }
 
-        onToggleInlineTranscript: function(event) {
-            this.markItemAsWatched();
+      this.playSelection();
+    },
 
-            if (event) event.preventDefault();
-            var $transcriptBodyContainer = this.$(".media__transcript-body-inline");
-            var $button = this.$(".media__transcript-btn-inline");
-            var $buttonText = this.$(".media__transcript-btn-inline .media__transcript-btn-text");
+    onToggleInlineTranscript: function(event) {
+      this.markItemAsWatched();
 
-            if ($transcriptBodyContainer.hasClass("inline-transcript-open")) {
-                $transcriptBodyContainer.stop(true,true).slideUp(function() {
-                    $(window).resize();
-                });
-                $button.attr('aria-expanded', false);
-                $transcriptBodyContainer.removeClass("inline-transcript-open");
-                $buttonText.html(this.model.get("_transcript").inlineTranscriptButton);
-            } else {
-                $transcriptBodyContainer.stop(true,true).slideDown(function() {
-                    $(window).resize();
-                });
-                $button.attr('aria-expanded', true);
-                $transcriptBodyContainer.addClass("inline-transcript-open");
-                $buttonText.html(this.model.get("_transcript").inlineTranscriptCloseButton);
+      if (event) event.preventDefault();
+      var $transcriptBodyContainer = this.$(".media__transcript-body-inline");
+      var $button = this.$(".media__transcript-btn-inline");
+      var $buttonText = this.$(".media__transcript-btn-inline .media__transcript-btn-text");
 
-                this.checkCompletionByTranscript();
-            }
-        },
+      if ($transcriptBodyContainer.hasClass("inline-transcript-open")) {
+        $transcriptBodyContainer.stop(true,true).slideUp(function() {
+          $(window).resize();
+        });
+        $button.attr('aria-expanded', false);
+        $transcriptBodyContainer.removeClass("inline-transcript-open");
+        $buttonText.html(this.model.get("_transcript").inlineTranscriptButton);
+      } else {
+        $transcriptBodyContainer.stop(true,true).slideDown(function() {
+          $(window).resize();
+        });
+        $button.attr('aria-expanded', true);
+        $transcriptBodyContainer.addClass("inline-transcript-open");
+        $buttonText.html(this.model.get("_transcript").inlineTranscriptCloseButton);
 
-        onExternalTranscriptClicked: function(event) {
-            this.markItemAsWatched();
-            this.checkCompletionByTranscript();
-        },
-    }, {
-        template: "mediawithbenefits"
-    });
+        this.checkCompletionByTranscript();
+      }
+    },
 
-    return Adapt.register('mediawithbenefits', {
-        model:MediaWithBenefitsModel,
-        view:MediaWithBenefitsView
-    });
+    onExternalTranscriptClicked: function(event) {
+      this.markItemAsWatched();
+      this.checkCompletionByTranscript();
+    },
+  }, {
+    template: "mediawithbenefits"
+  });
+
+  return Adapt.register('mediawithbenefits', {
+    model:MediaWithBenefitsModel,
+    view:MediaWithBenefitsView
+  });
+
 });
